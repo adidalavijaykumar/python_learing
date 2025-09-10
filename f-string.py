e030ce6b335e3a3a4791b4e4780a9ae9e6582175
adidalavijaykumar <vijaykumar.sunny9@gmail.com>
2025-09-10 20:35:17 +0530

    Add f-string.py

---
+++ f-string.py
@@ -0,0 +1,9 @@
+##Important topic
+
+name = "Alice"
+age = 30
+is_student = False
+
+print("My name is"+ name + ", I am" + "years old,and student status is"+ str(is_student)+".")
+
+print(f"My name is {name}, I am {age} years old, and student status {is_student}.")
